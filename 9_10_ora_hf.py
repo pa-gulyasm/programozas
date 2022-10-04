9f93a29ddf30f8de39b9d106c8dc035a64369627
pa-gulyasm <112852270+pa-gulyasm@users.noreply.github.com>
2022-10-04 13:37:29 +0200

    Create 9_10_ora_hf.py

---
+++ 9_10_ora_hf.py
@@ -0,0 +1,8 @@
+# hazifeladat
+elso_szam=int(input('Kérek egy számot 1-100-ig:'))
+masodik_szam=int(input('Kérek egy számot 1-100-ig:'))
+c=input("Összeadás vagy kivonás legyen?")
+if c=="+":
+  print(elso_szam+masodik_szam)
+elif c=="-":
+   print(elso_szam-masodik_szam) 
