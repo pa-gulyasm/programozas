806326d7d7e919c16ec06aba00e1218bcecd0433
pa-gulyasm <112852270+pa-gulyasm@users.noreply.github.com>
2022-11-15 13:16:05 +0100

    Create 11.13.hazi.py

---
+++ 11.13.hazi.py
@@ -0,0 +1,34 @@
+"""
+szám = 0
+while szám < 102:
+    print(szám)
+    szám = szám + 2
+print('Program Vége')
+# 1...100
+"""
+"""
+szám = 1
+while szám < 103:
+    print(szám)
+    szám = szám + 3
+print('Program Vége')
+# 1...100
+"""
+"""
+szam = 0
+while szam < 100:
+    if szam%2==1:
+        print(szam)
+    szam = szam + 1
+print('Program Vége')
+# páratlan számok
+"""
+"""
+szam = 0
+while szam < 102:
+    if szam%2==0:
+        print(szam)
+    szam = szam + 1
+print('Program Vége')
+# páros számok
+"""
